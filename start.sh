a1e6a7655d3af9dad3613f427080b41830721794
jay <jh37106@gmail.com>
2024-03-21 17:56:32 +0900

    주석없앰

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# # 첫 번째 명령어
-# echo "Making Django migrations..."
-# python manage.py makemigrations
+# 첫 번째 명령어
+echo "Making Django migrations..."
+python manage.py makemigrations
 
-# # 두 번째 명령어
-# echo "Applying migrations..."
-# python manage.py migrate
+# 두 번째 명령어
+echo "Applying migrations..."
+python manage.py migrate
 
 # 세 번째 명령어
 echo "Starting Django server..."
